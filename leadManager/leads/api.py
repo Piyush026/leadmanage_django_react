f5bf5d911c529449b9fd152a5ec6d314818949ba
Piyush026 <piyushjaiswal026@gmail.com>
2020-09-01 15:40:43 +0530

    first commit

---
+++ leadManager/leads/api.py
@@ -0,0 +1,13 @@
+from leads.models import Leads
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# lead Viewsets
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Leads.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
+    
